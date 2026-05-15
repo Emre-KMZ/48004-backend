e169f2e49c84588acb5ce07e50daba89f96f4f06
KeremBoncuk <kerem.boncuk@sabanciuniv.edu>
2026-05-15 05:09:59 +0300

    fix: use STORAGES dict for Django 5.0 and fix Minio addressing style

--- backend/ministore/settings.py
+++ backend/ministore/settings.py
@@ -86,7 +86,14 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 if env.str('MINIO_ENDPOINT', default=''):
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STORAGES = {
+        "default": {
+            "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        },
+        "staticfiles": {
+            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        },
+    }
     AWS_ACCESS_KEY_ID = env.str('MINIO_ACCESS_KEY')
     AWS_SECRET_ACCESS_KEY = env.str('MINIO_SECRET_KEY')
     AWS_STORAGE_BUCKET_NAME = env.str('MINIO_BUCKET')
@@ -94,5 +101,6 @@ if env.str('MINIO_ENDPOINT', default=''):
     AWS_S3_REGION_NAME = 'us-east-1'
     AWS_S3_FILE_OVERWRITE = False
     AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_ADDRESSING_STYLE = 'path'
     AWS_S3_CUSTOM_DOMAIN = env.str('MINIO_DOMAIN', default=f"localhost:9000/{AWS_STORAGE_BUCKET_NAME}")
     MEDIA_URL = f"http://{AWS_S3_CUSTOM_DOMAIN}/"
